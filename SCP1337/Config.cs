ef6e6e072d29a12a86a3bab03aa3eeaf11092904
Cass <60286224+Cass-dev-web@users.noreply.github.com>
2024-05-16 13:39:45 +0100

    tweak probability

--- SCP1337/Config.cs
+++ SCP1337/Config.cs
@@ -11,6 +11,6 @@ namespace SCP1337
 
         // Other...
         [Description("[IN PERCENTAGE (0-100%)] The probability that a player gets SCP-1337, drawn once per player that gets SCP, per round")]
-        public float SpawnProbability { get; set; } = 100;
+        public float SpawnProbability { get; set; } = 10;
     }
 }
